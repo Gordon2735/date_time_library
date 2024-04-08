63e0d0ff185849826dca1626ee73b8161e9098af
Team Webelistics™️, LLC <webelistics@gmail.com>
2024-04-08 03:34:15 -0400

    2024_04_08_03:34:15
    Signed-off-by: Team Webelistics™️, LLC <webelistics@gmail.com>

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+// Application Index.js File
+
+'use strict';
+
+// Create a Date() object for Christmas
+let christmas = new Date('December 25, 2021');
+
+// Get some details
+let day = getDay(christmas);
+let month = getMonth(christmas);
+console.log(day, month);
+
+// Modify the date
+addDays(christmas, 7);
+addYears(christmas, 1);
+console.log(christmas);
